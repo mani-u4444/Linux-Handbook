c9850907ba29acc9d9c551bf7eb9291edffc181a
mani-u4444 <mani.uber4444@gmail.com>
2025-12-01 19:52:42 +0530

    Update create_users.sh

--- Linux-Handbook/level-1-basic/users/create_users.sh
+++ Linux-Handbook/level-1-basic/users/create_users.sh
@@ -4,8 +4,8 @@ set -e
 echo "Creating group and users..."
 sudo groupadd devs || true
 
-sudo useradd -m -s /bin/bash -G devs alice || true
-sudo useradd -m -s /bin/bash -G devs bob || true
-sudo useradd -m -s /bin/bash -G devs ciuser || true
+sudo useradd -m -s /bin/bash -G devs user1 || true
+sudo useradd -m -s /bin/bash -G devs user2 || true
+sudo useradd -m -s /bin/bash -G devs user3 || true
 
 echo "Users created. Set passwords manually with: passwd <username>"
