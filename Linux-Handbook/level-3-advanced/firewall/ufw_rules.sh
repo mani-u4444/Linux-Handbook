df00dd9b15f93100b03b0873ade1564226da2788
mani-u4444 <mani.uber4444@gmail.com>
2025-12-02 18:16:53 +0530

    Update ufw_rules.sh

--- Linux-Handbook/level-3-advanced/firewall/ufw_rules.sh
+++ Linux-Handbook/level-3-advanced/firewall/ufw_rules.sh
@@ -1,9 +1,12 @@
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
 
-sudo ufw allow 2222/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 8080/tcp
+# Add permanent allow rules for specific ports
+sudo firewall-cmd --permanent --add-port=2222/tcp
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --permanent --add-port=8080/tcp
 
-sudo ufw enable
+# Reload firewalld to activate the new rules
+sudo firewall-cmd --reload
+
+# Verify the changes
+sudo firewall-cmd --list-all
